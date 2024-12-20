265d42158eab576e154accdc45223631336e7cdb
Elias Häußler <elias@haeussler.dev>
2024-12-20 19:15:31 +0100

    [TASK] Drop support for PHP 8.1

--- rector.php
+++ rector.php
@@ -27,7 +27,7 @@ use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
 use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void {
-    Config::create($rectorConfig, PhpVersion::PHP_81)
+    Config::create($rectorConfig, PhpVersion::PHP_82)
         ->in(
             __DIR__.'/src',
             __DIR__.'/tests',
