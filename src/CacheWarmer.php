ea4bfb18167d850865a98ab0a7fcd114ab538d84
Elias Häußler <elias@haeussler.dev>
2025-10-13 06:58:10 +0200

    [RELEASE] Release of cache-warmup 4.3.2

--- src/CacheWarmer.php
+++ src/CacheWarmer.php
@@ -41,7 +41,7 @@ use function is_string;
  */
 final class CacheWarmer
 {
-    public const VERSION = '4.3.1';
+    public const VERSION = '4.3.2';
 
     /**
      * @var array<string, Sitemap\Url>
